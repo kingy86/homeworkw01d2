6d89b71925894d0ca11b8ece3e6a12266dcc3b39
user <user@users-MacBook-Pro-5.local>
2019-02-26 16:50:09 +0000

    functions lab

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,54 @@
+def return_10()
+  return 10
+end
+
+def add(number_1, number_2)
+  return number_1 + number_2
+end
+
+def subtract(number_1, number_2)
+  return number_1 - number_2
+end
+
+def multiply(number_1, number_2)
+  return number_1 * number_2
+end
+
+def divide(number_1, number_2)
+  return number_1 / number_2
+end
+
+def length_of_string(test_string)
+  return test_string.length
+end
+
+def join_string(string_1, string_2)
+  return string_1 + string_2
+end
+
+def add_string_as_number(string_1, string_2)
+  add = string_1.to_i + string_2.to_i
+  return add
+end
+
+def number_to_full_month_name(month)
+  case month
+  when 1
+    return "January"
+  when 3
+    return "March"
+  when 9
+    return "September"
+  end
+end
+
+def number_to_short_month_name(short_month)
+  case short_month
+  when first_month_string = 1
+    return "Jan"
+  when fourth_month_string = 4
+    return "Apr"
+  when tenth_month_string = 10
+    return "Oct"
+  end
+end
